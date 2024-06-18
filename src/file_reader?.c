6ca8a4862ab5a9732710b9dad11a1041ae142639
xses <wiktort52@gmail.com>
2024-06-18 12:39:00 +0200

    Added all of the vertices

--- src/file_reader?.c
+++ src/file_reader?.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_ROWS 100
-#define MAX_COLS 3
+#define MAX_COLS 2
 
 int main() {
     FILE *file;
-    char line[1024];
+    char line[2048];
     int data[MAX_ROWS][MAX_COLS];
     int row = 0, col, value;
-    char *token;
+    char token[20];
+	int pointer;
+	int pointer2;
 
     // Open the file
-    file = fopen("data.txt", "r");
+    file = fopen("../verticies.csv", "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
@@ -23,17 +26,26 @@ int main() {
         col = 0;
 
         // Check if the line contains a single number
-        if (sscanf(line, "%d", &value) == 1 && value < 100) {
+        if (strlen(line) <= 5) {
+			sscanf(line, "%d", &value); 
+
             // It's a new set, reset the column index and increment the row index
             data[row++][col] = value;
+			data[row - 1][1] = 666;
+
         } else {
             // Tokenize the line using ';' as the delimiter
-            token = strtok(line, ";");
-            while (token != NULL && col < MAX_COLS) {
-                // Convert token to integer and store it in the array
-                data[row][col++] = atoi(token);
-                token = strtok(NULL, ";");
-            }
+			for (pointer = 0; line[pointer] != ';'; pointer++){}
+			strncpy(token, line, pointer); 
+			data[row][col++] = atoi(token);
+
+			printf("The first: %s %d %d %d \n", token, atoi(token), pointer, pointer2);
+
+			for (pointer2 = pointer + 1; line[pointer2] != ';'; pointer2++){}
+			strncpy(token, line + pointer + 1, pointer2 - pointer);
+			data[row][col++] = atoi(token);
+
+			printf("The shit: %s %d %d %d \n", token, atoi(token), pointer, pointer2);
             row++;
         }
     }
